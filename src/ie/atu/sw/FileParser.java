d18f242837e97256d20fd9c0fef38dc44c9df948
Joe1Jun <junker91joe@gmail.com>
2024-07-20 16:53:19 +0200

    Added instance variables to the FileParser class. Created an FileParser object as an instance variable of the Menu class to call methods in the FileParser class

--- src/ie/atu/sw/FileParser.java
+++ src/ie/atu/sw/FileParser.java
@@ -2,4 +2,20 @@ package ie.atu.sw;
 
 public class FileParser {
 
+	private static final int NUM_FEATURES = 50;
+	private String[] words;
+	private double[][] embeddings;
+
+	public void parseFile(String filePath) throws Exception {
+
+	}
+
+	public String[] getWords() {
+		return words;
+	}
+
+	public double[][] getEmbeddings() {
+		return embeddings;
+	}
+
 }
--- src/ie/atu/sw/Menu.java
+++ src/ie/atu/sw/Menu.java
@@ -4,8 +4,10 @@ import java.util.Scanner;
 
 public class Menu {
 	
+	FileParser parser = new FileParser();
 	// use boolean variable as an instance variable to be used by the class
 	private boolean keepRunning = true;
+	
 	private Scanner input;
 	
 	//initialize scanner that takes a scanner object as input
@@ -16,17 +18,21 @@ public class Menu {
 	
 	
  //method thats called by menu object in Runner class to start the menu
-	public void start()  {
+	public void start() throws Exception {
 		// loop keeps calling the showOptions method until the user selects the quit option
 		while(keepRunning) {
 			//call showOptions method to display menu.
 			showOptions();
 		    // reads the user input parses it to an integer and stores it in the variable choice
-			int choice = Integer.parseInt(input.next());
+			// //Attempt to call process method handling any exceptions that may occur 
+			 try {
+	                int choice = Integer.parseInt(input.next());
+	                processChoice(choice); }
 			//call process choice method to deal with the user input.
-			processChoice(choice);
+	                catch (NumberFormatException e) {
+		                System.out.println("Invalid input. Please enter a number between 1 and 8.");
 			
-			
+	                }
 			
 		}
 		
@@ -78,31 +84,44 @@ public class Menu {
 	}
 	}
 	
-	
+	//Most of these methods will take user input and call methods in different classes 
+	//using objects of those classes to do so 
 	private void parseFile() {
 		
 		System.out.println("Enter the file path for the embeddings file to be parsed");
 		String filePath = input.next();
-		
+		try {
+			parser.parseFile(filePath);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 	
 	private void specifyOutputFile() {
 		
+		System.out.println("Enter the output file path");
+		String outputFilePath = input.next();
 		
 	}
 	
 	private void enterWordOrText() {
 		
+		System.out.println("Enter a word");
+		String comparisonWord = input.next();
 		
 	}
 	
 	private void configureOptions () {
 		
+		System.out.println("How many top matches would you like ");
+		int topMatches = input.nextInt();
 		
 	}
 	
 	private void  findTopMatches () {
+		
 		
 		
 	}
