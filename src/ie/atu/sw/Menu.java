80d6474418dfedd90fc47f3caddba6878863a643
Joe1Jun <junker91joe@gmail.com>
2024-07-20 16:19:21 +0200

    Added more items to the user menu. Included a while loop in the start method and a switch statement to process user input

--- src/ie/atu/sw/Menu.java
+++ src/ie/atu/sw/Menu.java
@@ -1,5 +1,75 @@
 package ie.atu.sw;
 
-public class Menu {
+import java.util.Scanner;
 
+public class Menu {
+	
+	// use boolean variable as an instance variable to be used by the class
+	private boolean keepRunning = true;
+	private Scanner input;
+	
+	//initialize scanner that takes a scanner object as input
+	public Menu(Scanner input) {
+		
+		this.input = input;
+	}
+	
+	
+ //method thats called by menu object in Runner class to start the menu
+	public void start()  {
+		// loop keeps calling the showOptions method until the user selects the quit option
+		while(keepRunning) {
+		 // reads the user input parses it to an integer and stores it in the variable choice
+			int choice = Integer.parseInt(input.next());
+			//user switch statement on the user selection and the call methods based on that selection
+			switch(choice) {
+			
+			case 1 -> specifyEmbeddingFile();
+			case 2 -> specifyOutputFile();
+			case 3 -> enterWordOrText ();
+			case 4 -> configureOptions ();
+			case 5 -> findTopMatches ();
+			case 6 -> outputTopMatchesToFile();
+			case 7 -> printParsedWords ();
+			case 8 -> keepRunning = false;
+				
+				
+			} 
+			
+			
+		}
+		
+		
+	}
+
+	//this method prints the user menu when called
+	private void showOptions() {
+		
+		System.out.println(ConsoleColour.GREEN);
+		System.out.println("************************************************************");
+		System.out.println("*     ATU - Dept. of Computer Science & Applied Physics    *");
+		System.out.println("*                                                          *");
+		System.out.println("*          Similarity Search with Word Embeddings          *");
+		System.out.println("*                                                          *");
+		System.out.println("************************************************************");
+		System.out.println("(1) Specify Embedding File");
+		System.out.println("(2) Specify an Output File (default: ./out.txt)");
+		System.out.println("(3) Enter a Word or Text");
+		System.out.println("(4) Configure Options");
+		System.out.println("(5) Find Top Matches");
+		System.out.println("(6) Output top matches to a file");
+		System.out.println("(7) Print Parsed Words");
+		System.out.println("(8) Quit");
+
+		
+		//Output a menu of options and solicit text from the user
+		System.out.print(ConsoleColour.BLACK_BOLD_BRIGHT);
+		System.out.print("Select Option [1-?]>");
+		System.out.println();
+		
+	}
+	
+	
+	
+	
 }
--- src/ie/atu/sw/Runner.java
+++ src/ie/atu/sw/Runner.java
@@ -1,33 +1,78 @@
 package ie.atu.sw;
 
+//import java scanner
 import java.util.Scanner;
 
 public class Runner {
 	
 public static void main(String[] args) throws Exception {
 		
+	    // Create scanner object
 		Scanner scanner = new Scanner(System.in);
+		
+		//Create menu object passing scanner object to it 
+        Menu menu = new Menu(scanner);
         
+        //Attempt to call start method from menu class handling any exceptions that may occur
+        try {
+        	 menu.start();
+        } catch (Exception e) {
+        	//Print the stack trace if an exception occurs
+            e.printStackTrace();
+        }
 		
 		
 		
 
 		
 
-		// You may want to include a progress meter in you assignment!
-		System.out.print(ConsoleColour.YELLOW); // Change the colour of the console text
-		int size = 100; // The size of the meter. 100 equates to 100%
-		for (int i = 0; i < size; i++) { // The loop equates to a sequence of processing steps
-			printProgress(i + 1, size); // After each (some) steps, update the progress meter
-			Thread.sleep(10); // Slows things down so the animation is visible
+        // Change the colour of the console text
+		System.out.print(ConsoleColour.YELLOW);
+		 // The size of the meter. 100 equates to 100%
+		int size = 100;
+		// The loop equates to a sequence of processing steps
+		for (int i = 0; i < size; i++) { 
+			// After each (some) steps, update the progress meter
+			printProgress(i + 1, size); 
+			 // Slows things down so the animation is visible
+			Thread.sleep(10);
 		}
 	}
 
-	public static int printProgress(int a, int b) {
-
-		return 0;
-
-	}
+  //Print progress method will be implemented at the end******
+public static void printProgress(int index, int total) {
+	if (index > total) return;	//Out of range
+    int size = 50; 				//Must be less than console width
+    char done = '█';			//Change to whatever you like.
+    char todo = '░';			//Change to whatever you like.
+    
+    //Compute basic metrics for the meter
+    int complete = (100 * index) / total;
+    int completeLen = size * complete / 100;
+    
+    /*
+     * A StringBuilder should be used for string concatenation inside a 
+     * loop. However, as the number of loop iterations is small, using
+     * the "+" operator may be more efficient as the instructions can
+     * be optimized by the compiler. Either way, the performance overhead
+     * will be marginal.  
+     */
+    StringBuilder sb = new StringBuilder();
+    sb.append("[");
+    for (int i = 0; i < size; i++) {
+    	sb.append((i < completeLen) ? done : todo);
+    }
+    
+    /*
+     * The line feed escape character "\r" returns the cursor to the 
+     * start of the current line. Calling print(...) overwrites the
+     * existing line and creates the illusion of an animation.
+     */
+    System.out.print("\r" + sb + "] " + complete + "%");
+    
+    //Once the meter reaches its max, move to a new line.
+    if (done == total) System.out.println("\n");
+}
 
 	
 
