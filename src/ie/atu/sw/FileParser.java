2064a1ebc8703ce6220d6322e54816ce08a20310
Joe1Jun <junker91joe@gmail.com>
2024-07-23 16:11:30 +0300

    Added logic to parse file and populate arrays. Tried to parse the file unsuccessfully.

--- src/ie/atu/sw/FileParser.java
+++ src/ie/atu/sw/FileParser.java
@@ -22,19 +22,47 @@ public class FileParser {
 		words = new String [numLines];
 		embeddings = new double [numLines][NUM_FEATURES];
 	
+		//read the file again to populate arrays.
 		try (BufferedReader br = new BufferedReader( new FileReader(filePath))){
 			//initialise index to be used during the loop
 			int index = 0;
+			//variable to hold each line read from the array
+			String line ;
 			
-			
-			while(br.readLine() != null) {
+			//the line variable is equal to each line as its read.
+			while((line = br.readLine())!= null) {
 				
-				 String [] parsedWords =
+				//The read line including the words and the embeddings values
+				// are all stored as string values in the parsed words array.
+				//the split method splits the line at each space and stores this value in the array
+				//This means the first index , index 0 of the array will be the word as the word is at the start of each line
+				//This also means that the start index for the embedding on each line  is 1 which should be increased by 1 in a for loop to store the values in the embeddings array
+				String [] parsedWords = line.split(" ");
+				
+				//store the word in the words array
+				// the word at index [0] from each line is store in the words array
+				//at the current index which increases by one each round of the loop
+				
+				words[index] = parsedWords[0];
+				
+				//for loop to populate the embeddings array
+				//there are 50 features to loop through
+				for (int i = 0; i < NUM_FEATURES; i++) {
+					//loop iterates through the features and populates the the array 
+					//at the current index with the values that have been stored in the String array above
+					//this value at the index is stored along with the current feature value to populate the 2D array
+					//have to parse to double from the stored String values
+					
+					embeddings[index][i] = Double.parseDouble(parsedWords[i + 1]);
+					
+				}
 						 
-						 
-						 index++;
+			  index++;
 			}
 			
+			int numWords = words.length;
+			
+			System.out.println(numWords + "parsed");
 			
 		} catch (Exception e) {
 			e.printStackTrace();
