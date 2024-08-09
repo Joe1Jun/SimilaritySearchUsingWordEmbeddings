e885e0d1a6b751c7b4ff4d3038c808ae85c13016
Joe1Jun <junker91joe@gmail.com>
2024-08-09 14:51:30 +0200

    Time complexity was too high using 2D arrays so have to refactor the whole classto try and process words one at a time to avoid stack overflow error

--- src/ie/atu/sw/SentenceComparison.java
+++ src/ie/atu/sw/SentenceComparison.java
@@ -61,36 +61,41 @@ public class SentenceComparison {
 		// Call processSentence method to seperate the sentence into words and populate array;
 		sentenceWords = processSentence(sentence);
 
-		double wordEmbeddings [][] = getWordEmbeddings(sentenceWords);
 		
-		
-		
-		// Check if words and index are correct by printing word and associated embeddings
-		for(int i = 0; i < sentenceWords.length; i++) {
-			System.out.print("index of word :" + sentenceWords[i] );
-			// In the inner loop wordEmbeddings at i is equal to the index 
-			for(int j = 0; j < wordEmbeddings[i].length; j++) {
-				System.out.print("   embeddings :" + wordEmbeddings[i][j]);
-			}
-			System.out.println();
+		for(int i =0; i < sentenceWords.length; i++) {
+			
+			
+			double wordEmbeddings [] = getWordEmbeddings(sentenceWords[i]);
+			
 		}
 		
 		
-		double [][] similarities = similarityScores(wordEmbeddings);
 		
-		// Print similarity scores for debugging
-	    System.out.println("Similarity Scores:");
-	    for (int i = 0; i < similarities.length; i++) {
-	        System.out.println("Similarities for word '" + sentenceWords[i] + "': ");
-	        for (int j = 0; j < similarities[i].length; j++) {
-	            System.out.println(similarities[i][j] + " ");
-	        }
-	        System.out.println();
-	    }
+		
+		
+		
+		// Manually copy the words and similarities arrays before sorting
+				// This preserves the original words array as the instance variable shouldnt change
+				// Without this modification the words array would be altered and not correspond to its embeddings .
+				// All subsequent word searches would be incorrect.
+		        String[] wordsCopy = copyArray(words);
+		
+		     // Process and print top matches for each word in the sentence
+		        
+	   
 
 		topSentences = constructSentences(allTopMacthes);
 
 		return topSentences;
+	}
+	
+	private void printTopMatches(String originalWord, double[][] similarities, String[] words, int topN) {
+	    System.out.println("Top " + topN + " matches for '" + originalWord + "':");
+	    for (int i = 0; i < Math.min(topN, similarities.length); i++) {
+	        int originalIndex = (int) similarities[i][1];
+	        System.out.printf("%d. %s (similarity: %.4f)%n", i + 1, words[originalIndex], similarities[i][0]);
+	    }
+	    System.out.println();
 	}
 
 	private String[] constructSentences(String[][] allTopWordMatches) {
@@ -140,22 +145,119 @@ public class SentenceComparison {
 	    return similaritiesOfWords;
 	}
 
+	
+	
+	private void quickSort(double[]  similarities, String[] words, int low, int high) {
+
+		// Checks if the first index is less than the pivot
+		if (low < high) {
+
+			// The index of the pivot is returned from the partition function
+			// This value is used to in order to pass the values of the low and high back
+			// into the quick sort function
+			// This allows the function to sort the sub array to the left of the pivot and
+			// the sub array to the right.
+			int pi = partition(similarities, words, low, high);
+
+			// Recursively sort the left subarray
+			// The high value of the left subarray is set to the index before the pivot.
+			quickSort(similarities, words, low, pi - 1);
+			// Recursively sort the right subarray
+			// The low value is set to the index after the pivot.
+			quickSort(similarities, words, pi + 1, high);
+
+		}
+
+	}
+
+	private int partition(double [] similarities, String[] words, int low, int high) {
+
+		// Initialise the pivot at the last index of the similarities array
+		double pivot = similarities[high];
+
+		// Initialise the i variable at one value less than the first index
+
+		int i = low - 1;
+
+		// Use a for loop that starts at the low value and ends at the high value.
+		for (int j = low; j < high; j++) {
+
+			// Check if the value of similarities at j is greater than the pivot value.
+			// Because the end array must be in descending order.
+			// If the value is less than our pivot its ignored.
+			if (similarities[j], pivot) > 0) {
+
+				// i is incremented first as it is required to be at an index in the array.
+				// If the swap occurs before this it would trigger an out of bounds exception
+				// If the first element is greater than the pivot it will be essentially swapped
+				// with itself as i and j will be at the same index
+				i++;
+
+				// Initialise a double to temporarily store values to be swapped
+				// The double takes the value of similarities at the index of i
+				// Example array on second pass of loop [2.3][7.1] high [ 5.2]
+				// i is incremented on second pass of loop as first pass is ignored.
+
+				// temp is equal to 2.3
+				double  temp = similarities[i];
+				// Make value of index 0 7.1.
+				similarities[i] = similarities[j];
+				// Swap the value back in for j so index1 now has a value of 2.3
+				// Array at the end [7.1][2.3] high[5.2]
+				similarities[j] = temp;
+
+				// If the first double is greater than the pivot it will essentially be swapped
+				// with itself as both i and j will be at the same index
+
+				// Do the same for the words at the corresponding indexes
+				String wordTemp = words[i];
+				words[i] = words[j];
+				words[j] = wordTemp;
+
+			}
+
+		}
+
+		// Must now swap the pivot at high to its correct index.
+		// Create a temporary variable to hold the value of the index one up from the
+		// index of i. As the index of i after the loop will
+		// be the index of the last value lower than the pivot so need to increase the
+		// the index of i by one so the index is now on a value higher than the pivot.
+
+		double  tempSim = similarities[i + 1];
+		// Make the value of index i + 1 the same as the value of the pivot
+		similarities[i + 1] = similarities[high];
+		// Make the last index of the array the same value as the one stored in the
+		// temporary variable.
+		// This is the same value as the original i + 1 index before the swap.
+		// Now that the pivot is in the correct position with all values to the left
+		// being higher and to the right being lower.
+
+		similarities[high] = tempSim;
+
+		// Swap words[i+1] and words[high] (or pivot)
+		String tempWord = words[i + 1];
+		words[i + 1] = words[high];
+		words[high] = tempWord;
+
+		// The index of the pivot will be passed back to the quickSort function so the
+		// sub arrays to the left and right of the pivot
+		// can be passed back into the quicksort function.
+		return i + 1;
+
+	}
+	
+	
+	
 
 	
-	private double[][] getWordEmbeddings(String [] sentenceWords) {
-		//declare the 2D array to store the wordEmbeddings associated with the embeddings
-		// make the number of rows the length of the number of workIndexes from the sentence
-		// Do not need to specify the column as this will be dynamically determined based on the actual embeddings.
+	private double[] getWordEmbeddings(String word) {
 		
-	    double[][] wordEmbeddings = new double[sentenceWords.length][];
-	    for(int i = 0; i < sentenceWords.length; i++) {
-	    	
-	    	wordEmbeddings[i] = wordComparison.getEmbedding(sentenceWords[i]);
-	    	
-	    }
+	
+	   double[] wordEmbedding = wordComparison.getEmbedding(word);
+	    
 
-
-	    return wordEmbeddings;
+	    return wordEmbedding;
 	}
 
 	
@@ -186,5 +288,15 @@ public class SentenceComparison {
 
 		return dotProduct / (Math.sqrt(normA) * Math.sqrt(normB));
 	}
+	
+	
+	 // Method to manually copy a String array
+    private String[] copyArray(String[] array) {
+        String[] copy = new String[array.length];
+        for (int i = 0; i < array.length; i++) {
+            copy[i] = array[i];
+        }
+        return copy;
+    }
 
 }
